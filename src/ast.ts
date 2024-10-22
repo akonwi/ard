be206582078adf09f320b9ce1ab88bb853c2442c
Akonwi Ngoh <akonwi@gmail.com>
2024-10-22 17:45:02 -0400

    initial AST defs for expressions

---
+++ src/ast.ts
@@ -0,0 +1,34 @@
+// AST.ts
+
+import type { Token } from "./lexer/lexer";
+
+type Primary = { type: "Primary"; token: Token };
+
+type Literal = { type: "Literal"; token: Token };
+type Grouping = { type: "Grouping"; expression: Expr };
+type Unary = {
+	type: "Unary";
+	operator: Token;
+	right: Expr;
+};
+type Binary = {
+	type: "Binary";
+	left: Expr;
+	operator: Token;
+	right: Expr;
+};
+
+export type Expr = Literal | Grouping | Unary | Binary;
+// | { type: "Variable"; name: Token }
+// | { type: "Assign"; name: Token; value: Expr }
+// | { type: "Call"; callee: Expr; paren: Token; arguments: Expr[] };
+
+export type Stmt =
+	| { type: "Expression"; expression: Expr }
+	| { type: "Let"; name: Token; initializer: Expr | null }
+	| { type: "Mut"; name: Token; initializer: Expr | null }
+	| { type: "Block"; statements: Stmt[] }
+	| { type: "If"; condition: Expr; thenBranch: Stmt; elseBranch: Stmt | null }
+	| { type: "While"; condition: Expr; body: Stmt }
+	| { type: "Function"; name: Token; params: Token[]; body: Stmt[] }
+	| { type: "Return"; keyword: Token; value: Expr | null };
