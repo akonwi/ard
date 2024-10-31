5f58158f80d0c624ba43cd721ded30ef8d25c605
Akonwi Ngoh <akonwi@gmail.com>
2024-10-30 22:20:40 -0400

    generating function defs and calls

--- src/generator/tree-sitter-generator.js
+++ src/generator/tree-sitter-generator.js
@@ -13,7 +13,15 @@ function generateNode(node) {
 			return node.children.map(generateNode).join("\n");
 		}
 		case "statement": {
-			return node.children.map(generateNode).join("\n");
+			if (node.childCount > 1)
+				throw new Error(
+					"Multiple statements encountered at " + node.startPosition,
+				);
+			if (node.firstChild == null)
+				throw new Error("Empty statement at " + node.startPosition);
+			let raw = generateNode(node.firstChild);
+			if (node.firstChild.type !== "function_definition") raw += ";";
+			return raw;
 		}
 		case "variable_definition": {
 			const binding = node.childForFieldName("binding");
@@ -31,7 +39,59 @@ function generateNode(node) {
 				);
 			}
 			const letConst = binding.text === "mut" ? "let" : "const";
-			return `${letConst} ${name.text} = ${generateNode(value)};`;
+			return `${letConst} ${name.text} = ${generateNode(value)}`;
+		}
+		case "function_definition": {
+			const nameNode = node.childForFieldName("name");
+			const parameters = node.childForFieldName("parameters");
+			const body = node.childForFieldName("body");
+			if (nameNode == null)
+				throw new Error("Missing function name at " + node.startPosition);
+			if (parameters == null)
+				throw new Error("Missing function parameters at " + node.startPosition);
+			if (body == null)
+				throw new Error("Missing function body at " + node.startPosition);
+			return `function ${nameNode.text}${generateNode(parameters)} ${generateNode(body)}`;
+		}
+		case "parameters": {
+			return `(${node.namedChildren.map(generateNode).join(", ")})`;
+		}
+		case "param_def": {
+			const name = node.childForFieldName("name");
+			if (name == null)
+				throw new Error("Missing parameter name at " + node.startPosition);
+			return `${name.text}`;
+		}
+		case "block": {
+			if (node.namedChildCount === 0) return "{}";
+			let raw = `{\n`;
+			node.namedChildren.forEach((child, index) => {
+				// return the result of the last statement
+				const isLast = index === node.namedChildren.length - 1;
+				raw += `\t${isLast ? "return " : ""}${generateNode(child)}\n`;
+			});
+			raw += "}";
+			return raw;
+		}
+		case "function_call": {
+			const target = node.childForFieldName("target");
+			const args = node.childForFieldName("arguments");
+			if (target == null)
+				throw new Error(
+					"Missing function name to call at" + node.startPosition,
+				);
+			if (args == null)
+				throw new Error(
+					"Missing function arguments for call at " + node.startPosition,
+				);
+
+			return `${generateNode(target)}${generateNode(args)}`;
+		}
+		case "paren_arguments": {
+			return `(${node.namedChildren.map(generateNode).join(", ")})`;
+		}
+		case "identifier": {
+			return node.text;
 		}
 		case "primitive_value": {
 			const child = node.firstChild;
--- src/generator/tree-sitter-generator.test.js
+++ src/generator/tree-sitter-generator.test.js
@@ -5,6 +5,8 @@ const fs = require("node:fs");
 const { generateJavascript } = require("./tree-sitter-generator");
 const { makeParser } = require("../parser/tree-sitter-parser");
 
+const parser = makeParser();
+
 const variablesKon = fs.readFileSync(
 	require.resolve("../samples/variables.kon"),
 	"utf8",
@@ -12,11 +14,24 @@ const variablesKon = fs.readFileSync(
 const variablesJS = fs
 	.readFileSync(require.resolve("../samples/variables.js"), "utf8")
 	.trimEnd();
+const functionsKon = fs.readFileSync(
+	require.resolve("../samples/functions.kon"),
+	"utf8",
+);
+const functionsJS = fs
+	.readFileSync(require.resolve("../samples/functions.js"), "utf8")
+	.trimEnd();
 
 describe("generating javascript", () => {
 	test("variable definitions", () => {
-		const tree = makeParser().parse(variablesKon);
+		const tree = parser.parse(variablesKon);
 		const result = generateJavascript(tree);
 		assert.equal(result, variablesJS, "generated code does not match");
+	});
+
+	test("function definitions and calls", () => {
+		const tree = parser.parse(functionsKon);
+		const result = generateJavascript(tree);
+		assert.equal(result, functionsJS, "generated code does not match");
 	});
 });
