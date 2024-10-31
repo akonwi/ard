a936dd7cb53f618f7cfb7728ba7b2fabc68713ad
Akonwi Ngoh <akonwi@gmail.com>
2024-10-31 16:39:27 -0400

    remove old code

--- src/parser/tree-sitter-parser.test.js
+++ src/parser/tree-sitter-parser.test.js
@@ -1,11 +1,11 @@
+// @ts-check
 const assert = require("node:assert");
 const { describe, test } = require("node:test");
-const { parser } = require("./tree-sitter-parser");
-// @ts-check
+const { makeParser } = require("./tree-sitter-parser");
 
 describe("tree-sitter parser", () => {
 	test("walking the tree", () => {
-		const tree = parser.parse("let x: Num = 5");
+		const tree = makeParser().parse("let x: Num = 5");
 		assert(tree != null, "tree is null");
 	});
 });
