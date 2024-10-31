73246f74a0dd735889249742bdb2a0eb2fd58ced
Akonwi Ngoh <akonwi@gmail.com>
2024-10-31 00:40:47 -0400

    npm run kon

---
+++ src/run.js
@@ -0,0 +1,12 @@
+// @ts-check
+const { readFileSync } = require("node:fs");
+const { makeParser } = require("./parser/tree-sitter-parser");
+const { generateJavascript } = require("./generator/tree-sitter-generator");
+
+/** @type (input: string) => string **/
+function compile(input) {
+	return generateJavascript(makeParser().parse(input));
+}
+
+const kon = compile(readFileSync(process.argv[2], "utf8"));
+eval(kon);
