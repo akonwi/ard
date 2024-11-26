1dbdd308c48f520451dd18973828c694d94af3c2
Akonwi Ngoh <akonwi@gmail.com>
2024-11-25 22:28:49 -0500

    add test

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -155,6 +155,31 @@ func TestVariableDeclarations(t *testing.T) {
 			},
 		},
 		{
+			name:  "List elements must match declared type",
+			input: `let strings: [Str] = [1, 2, 3]`,
+			output: &Program{
+				Statements: []Statement{
+					&VariableDeclaration{
+						Mutable:      false,
+						Name:         "strings",
+						Type:         &checker.ListType{ItemType: checker.StrType},
+						InferredType: &checker.ListType{ItemType: checker.NumType},
+						Value: &ListLiteral{
+							Type: &checker.ListType{ItemType: checker.NumType},
+							Items: []Expression{
+								&NumLiteral{Value: "1"},
+								&NumLiteral{Value: "2"},
+								&NumLiteral{Value: "3"},
+							},
+						},
+					},
+				},
+			},
+			diagnostics: []checker.Diagnostic{
+				{Msg: "Type mismatch: expected [Str], got [Num]"},
+			},
+		},
+		{
 			name:  "Valid list",
 			input: `let numbers: [Num] = [1, 2, 3]`,
 			output: &Program{
