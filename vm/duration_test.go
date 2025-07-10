b70e6933525f9b2555af491d6583e900e579fcd2
Akonwi Ngoh <akonwi@gmail.com>
2025-07-09 21:17:48 -0400

    Update duration_test.go

--- vm/duration_test.go
+++ vm/duration_test.go
@@ -12,5 +12,21 @@ func TestDurationFunctions(t *testing.T) {
 			`,
 			want: 20 * 1000,
 		},
+		{
+			name: "from_minutes",
+			input: `
+				use ard/duration
+				duration::from_minutes(5)
+			`,
+			want: 5 * 60 * 1000,
+		},
+		{
+			name: "from_hours",
+			input: `
+				use ard/duration
+				duration::from_hours(2)
+			`,
+			want: 2 * 3600 * 1000,
+		},
 	})
 }
