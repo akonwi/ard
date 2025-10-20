155307db71147869802e281bcb7c22462c25b9db
Akonwi Ngoh <akonwi@gmail.com>
2025-10-20 17:27:45 -0400

    don't allow any outer variable references

--- checker/async_test.go
+++ checker/async_test.go
@@ -9,7 +9,7 @@ import (
 func TestFibers(t *testing.T) {
 	run(t, []test{
 		{
-			name: "Fibers cannot reference mutable variables in outer scopes",
+			name: "Fibers cannot reference mutable variables in outer scope",
 			input: `
 			use ard/async
 
@@ -24,7 +24,7 @@ func TestFibers(t *testing.T) {
 			},
 		},
 		{
-			name: "Fibers can reference read-only variables in outer scopes",
+			name: "Fibers cannot reference read-only variables in outer scope",
 			input: `
 			use ard/async
 
@@ -33,7 +33,10 @@ func TestFibers(t *testing.T) {
 				duration + 1
 			})
 			`,
-			diagnostics: []checker.Diagnostic{},
+			diagnostics: []checker.Diagnostic{
+				/* todo: need to be more specific about how the mutable reference won't be shared */
+				{Kind: checker.Error, Message: "Undefined variable: duration"},
+			},
 		},
 		{
 			name: "Valid fiber functions work",
