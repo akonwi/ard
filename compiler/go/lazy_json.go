087e1b48ceba39750e1f9d23c521b9c14b151b6c
Akonwi Ngoh <akonwi@gmail.com>
2026-04-28 03:01:36 -0400

    Use cap 12 for lazy int-list parser
    Result: {"status":"keep","go_decode_ms":25.03275,"build_ms":3566.61,"vm_output":29678473,"go_output":29678473,"binary_size_bytes":22957074}

--- compiler/go/lazy_json.go
+++ compiler/go/lazy_json.go
@@ -144,7 +144,7 @@ func decodeLazyJSONIntList(s string) ([]int, bool) {
 		return nil, false
 	}
 	idx++
-	out := make([]int, 0, 16)
+	out := make([]int, 0, 12)
 	for {
 		idx = skipJSONSpaces(s, idx)
 		if idx >= len(s) {
